ce4fff094ba38abe60449f881b8565d5ec5ea791
samcooper720x <samcooper@sam.home>
2021-05-14 22:09:03 +0100

    Chapter 04, Privacy

--- the_money_example/money.py
+++ the_money_example/money.py
@@ -1,6 +1,10 @@
 class Dollar:
     def __init__(self, amount):
-        self.amount = amount
+        # Although in the book Kent Beck eventually makes the amount
+        # instance variable private, he is working in Java. Doing so
+        # here in Python didn't seem appropiate. 
+
+        self.amount = amount 
 
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
--- the_money_example/test_money.py
+++ the_money_example/test_money.py
@@ -5,11 +5,13 @@ from money import Dollar
 
 class TestMultiCurrencyMoney:
     def test_multiplication(self):
+        # In the book, Kent Beck compares the two class instances
+        # for equality. In Python we can only do those by defining
+        # an __eq__ method, so to keep as close to the example as
+        # possible I've chosen to compare the specific attributes.
         five = Dollar(5)
-        product = five.times(2)
-        assert 10 == product.amount
-        product = five.times(3)
-        assert 15 == product.amount
+        assert Dollar(10).amount == five.times(2).amount
+        assert Dollar(15).amount == five.times(3).amount
 
     def test_equality(self):
         assert Dollar(5).equals(Dollar(5))
