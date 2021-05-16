0921ea79b7c5bb4fb677ec142e114c39fcbb902a
samcooper720x <samcooper@sam.home>
2021-05-16 12:31:20 +0100

    Chapter 18, First Steps to xUnit

---
+++ the_xUnit_example/test_xUnit.py
@@ -0,0 +1,12 @@
+from xUnit import WasRun, TestCase
+
+
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun('test_method')
+        assert(not test.was_run)
+        test.run()
+        assert(test.was_run)
+
+
+TestCaseTest('test_running').run()
---
+++ the_xUnit_example/xUnit.py
@@ -0,0 +1,17 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = None
+        TestCase.__init__(self, name)
+
+    def test_method(self):
+        self.was_run = 1
+
